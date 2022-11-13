9293f6f16af27c8cac68debe595b52c38676b738
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-11-13 22:17:22 +0200

    Export class.

--- core/erdpy_core/__init__.py
+++ core/erdpy_core/__init__.py
@@ -1,4 +1,5 @@
 from erdpy_core.account import Account
 from erdpy_core.address import Address
+from erdpy_core.transaction import Transaction
 
-__all__ = ["Account", "Address"]
+__all__ = ["Account", "Address", "Transaction"]
