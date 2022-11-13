47795c2baeacc0c91a8d25a144a86b9c5af636b3
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-11-14 00:21:34 +0200

    Fix typing.

--- core/erdpy_core/transaction_payload.py
+++ core/erdpy_core/transaction_payload.py
@@ -8,28 +8,28 @@ class TransactionPayload(ITransactionPayload):
         self.data = data
 
     @classmethod
-    def empty(cls):
+    def empty(cls) -> 'TransactionPayload':
         return TransactionPayload(bytes())
 
     @classmethod
-    def from_str(cls, data: str):
+    def from_str(cls, data: str) -> 'TransactionPayload':
         """
         Creates a TransactionPayload from a utf-8 string.
         """
         return TransactionPayload(data.encode("utf-8"))
 
     @classmethod
-    def from_encoded_str(cls, encoded: str):
+    def from_encoded_str(cls, encoded: str) -> 'TransactionPayload':
         """
         Creates a TransactionPayload from a base-64 encoded string.
         """
         data = base64.b64decode(encoded)
         return TransactionPayload(data)
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return len(self.data) == 0
 
-    def length(self):
+    def length(self) -> int:
         return len(self.data)
 
     def encoded(self) -> str:
