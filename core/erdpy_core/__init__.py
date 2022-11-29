37e889f46ac5f277bfc409510f9ad6267352686c
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-11-29 14:31:19 +0200

    Fix exports.

--- core/erdpy_core/__init__.py
+++ core/erdpy_core/__init__.py
@@ -1,7 +1,8 @@
 from erdpy_core.account import Account
 from erdpy_core.address import Address
 from erdpy_core.function_call_builder import FunctionCallBuilder
+from erdpy_core.message import Message
 from erdpy_core.transaction import Transaction
 from erdpy_core.transaction_payload import TransactionPayload
 
-__all__ = ["Account", "Address", "Transaction", "FunctionCallBuilder", "TransactionPayload"]
+__all__ = ["Account", "Address", "Transaction", "FunctionCallBuilder", "TransactionPayload", "Message"]
