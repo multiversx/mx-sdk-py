ee67e6dc4fcbe1f4941918ae8131d714d3a665b6
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-12-21 22:15:32 +0200

    Adjust account nonce holder.

--- core/erdpy_core/account_test.py
+++ core/erdpy_core/account_test.py
@@ -1,11 +1,9 @@
 
 from erdpy_core.account import AccountNonceHolder
-from erdpy_core.address import Address
 
 
 def test_account_nonce_holder():
-    address = Address.from_bech32("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th")
-    account = AccountNonceHolder(address)
+    account = AccountNonceHolder(41)
     account.nonce = 42
     assert account.get_nonce_then_increment() == 42
     assert account.get_nonce_then_increment() == 43
