3f39903ded2a98350ae10016b182738d84c2f37f
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-01-04 10:58:33 +0200

    fix data field for transaction

--- network_providers/erdpy_network_providers/transactions.py
+++ network_providers/erdpy_network_providers/transactions.py
@@ -78,7 +78,11 @@ class TransactionOnNetwork:
 
         result.gas_price = response.get('gasPrice', 0)
         result.gas_limit = response.get('gasLimit', 0)
-        result.data = base64.b64decode(response.get('data', '')).decode()
+
+        data = response.get('data', '')
+        data = "" if data is None else data
+
+        result.data = base64.b64decode(data).decode()
         result.status = TransactionStatus(response.get('status'))
         result.timestamp = response.get('timestamp', 0)
 
