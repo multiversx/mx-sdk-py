15b57fff226eb8738dc8ab4444d54c0a7a60cbf6
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-06-15 13:21:05 +0300

    add nodes for delegation factory

--- core/multiversx_sdk_core/transaction_builders/delegation_factory.py
+++ core/multiversx_sdk_core/transaction_builders/delegation_factory.py
@@ -4,10 +4,12 @@ from multiversx_sdk_core import TransactionPayload
 from multiversx_sdk_core.address import Address
 from multiversx_sdk_core.constants import (ARGS_SEPARATOR,
                                            DELEGATION_MANAGER_SC_ADDRESS)
+from multiversx_sdk_core.errors import ErrListsLengthDoNotMatch
 from multiversx_sdk_core.interfaces import (IAddress, IChainID, IGasLimit,
-                                            IGasPrice, INonce,
+                                            IGasPrice, INonce, ISignature,
                                             ITransactionPayload,
-                                            ITransactionValue)
+                                            ITransactionValue,
+                                            IValidatorPublicKey)
 from multiversx_sdk_core.serializer import args_to_string
 from multiversx_sdk_core.transaction import Transaction
 
@@ -34,9 +36,27 @@ class ICreateNewDelegationContractArgs(IBaseArgs, Protocol):
     data: Optional[ITransactionPayload]
 
 
+class IAddNodesArgs(IBaseArgs, Protocol):
+    sender: IAddress
+    delegation_contract: IAddress
+    public_keys: List[IValidatorPublicKey]
+    signed_messages: List[ISignature]
+    data: Optional[ITransactionPayload]
+
+
 class MetaChainSystemSCsCost:
+    STAKE = 5000000
+    UNSTAKE = 5000000
+    UNBOND = 5000000
+    CLAIM = 5000000
+    GET = 5000000
+    CHANGE_REWARD_ADDRESS = 5000000
+    CHANGE_VALIDATOR_KEYS = 5000000
+    UNJAIL = 5000000
     DELEGATION_MANAGER_OPS = 50000000
     DELEGATION_OPS = 1000000
+    UNSTAKE_TOKENS = 5000000
+    UNBOND_TOKENS = 5000000
 
 
 class DelegationFactory:
@@ -67,6 +87,37 @@ class DelegationFactory:
         args_list: List[Any] = [args.total_delegation_cap, args.service_fee]
         data = ARGS_SEPARATOR.join([function, args_to_string(args_list)])
         return TransactionPayload.from_str(data)
+
+    def add_nodes(self, args: IAddNodesArgs):
+        num_of_nodes = len(args.public_keys)
+
+        if not args.data:
+            args.data = self._prepare_data_for_add_nodes(args)
+
+        if not args.gas_limit:
+            args.gas_limit = self._estimate_system_sc_call(args.data.length(), MetaChainSystemSCsCost.DELEGATION_OPS, num_of_nodes + 1)
+
+        transaction = self.create_transaction(
+            sender=args.sender,
+            receiver=args.delegation_contract,
+            gas_limit=args.gas_limit,
+            data=args.data,
+            gas_price=args.gas_price,
+            nonce=args.transaction_nonce,
+            value=args.value
+        )
+
+        return transaction
+
+    def _prepare_data_for_add_nodes(self, args: IAddNodesArgs) -> TransactionPayload:
+        if len(args.public_keys) != len(args.signed_messages):
+            raise ErrListsLengthDoNotMatch("The number of public keys should match the number of signed messages")
+
+        add_nodes_data = "addNodes"
+        for i in range(len(args.public_keys)):
+            add_nodes_data += f"@{args.public_keys[i].hex()}@{args.signed_messages[i].hex()}"
+
+        return TransactionPayload.from_str(add_nodes_data)
 
     def _estimate_system_sc_call(self, data_length: int, base_cost: int, factor: int = 1) -> int:
         gas_limit = self.config.min_gas_limit + data_length * self.config.gas_limit_per_byte
--- core/multiversx_sdk_core/transaction_builders/delegation_factory_test.py
+++ core/multiversx_sdk_core/transaction_builders/delegation_factory_test.py
@@ -1,7 +1,10 @@
+from multiversx_sdk_wallet import ValidatorSecretKey, ValidatorSigner
+
 from multiversx_sdk_core.address import Address
 from multiversx_sdk_core.constants import DELEGATION_MANAGER_SC_ADDRESS
+from multiversx_sdk_core.messages import ArbitraryMessage
 from multiversx_sdk_core.transaction_builders.delegation_factory import (
-    DelegationFactory, ICreateNewDelegationContractArgs,
+    DelegationFactory, IAddNodesArgs, ICreateNewDelegationContractArgs,
     MetaChainSystemSCsCost)
 
 
@@ -26,25 +29,61 @@ class NewDelegationContractArgs(ICreateNewDelegationContractArgs):
         self.gas_price = None
 
 
+class AddNodesArgs(IAddNodesArgs):
+    def __init__(self) -> None:
+        self.sender = Address.from_bech32("erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2")
+        self.delegation_contract = Address.from_bech32("erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqtllllls002zgc")
+
+        validator_secret_key = ValidatorSecretKey.from_string("7cff99bd671502db7d15bc8abc0c9a804fb925406fbdd50f1e4c17a4cd774247")
+        validator_signer = ValidatorSigner(validator_secret_key)
+
+        message = ArbitraryMessage(bytes.fromhex(self.delegation_contract.hex()))
+        signed_message = validator_signer.sign(message)
+        public_key = validator_secret_key.generate_public_key()
+
+        self.public_keys = [public_key]
+        self.signed_messages = [signed_message]
+
+        self.transaction_nonce = 777
+        self.data = None
+        self.gas_limit = None
+        self.gas_price = None
+        self.value = None
+
+
 class TestDelagationFactory:
     config = Config()
-    args = NewDelegationContractArgs()
     factory = DelegationFactory(config)
 
-    def test_compute_data_field(self):
-        data = self.factory._prepare_data_for_create_new_delegation_contract(self.args)
+    def test_compute_data_field_for_new_delegation_contract(self):
+        data = self.factory._prepare_data_for_create_new_delegation_contract(NewDelegationContractArgs())
         assert str(data) == "createNewDelegationContract@010f0cf064dd59200000@0a"
 
-    def test_compute_gas_limit(self):
-        data = self.factory._prepare_data_for_create_new_delegation_contract(self.args)
+    def test_compute_gas_limit_for_new_delegation_contract(self):
+        data = self.factory._prepare_data_for_create_new_delegation_contract(NewDelegationContractArgs())
         gas_limit = self.factory._estimate_system_sc_call(data.length(), MetaChainSystemSCsCost.DELEGATION_MANAGER_OPS, 2)
         assert gas_limit == 100126500
 
     def test_create_new_delegation_contract(self):
-        transaction = self.factory.create_new_delegation_contract(self.args)
+        transaction = self.factory.create_new_delegation_contract(NewDelegationContractArgs())
 
         assert transaction.sender.bech32() == "erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2"
         assert transaction.receiver.bech32() == DELEGATION_MANAGER_SC_ADDRESS
         assert transaction.chainID == "D"
         assert transaction.nonce == 777
         assert transaction.signature == b""
+        assert str(transaction.data) == "createNewDelegationContract@010f0cf064dd59200000@0a"
+
+    def test_compute_data_field_for_add_nodes(self):
+        data = self.factory._prepare_data_for_add_nodes(AddNodesArgs())
+        assert str(data) == "addNodes@e7beaa95b3877f47348df4dd1cb578a4f7cabf7a20bfeefe5cdd263878ff132b765e04fef6f40c93512b666c47ed7719b8902f6c922c04247989b7137e837cc81a62e54712471c97a2ddab75aa9c2f58f813ed4c0fa722bde0ab718bff382208@81109fa1c8d3dc7b6c2d6e65206cc0bc1a83c9b2d1eb91a601d66ad32def430827d5eb52917bd2b0d04ce195738db216"
+
+    def test_add_nodes(self):
+        transaction = self.factory.add_nodes(AddNodesArgs())
+
+        assert transaction.sender.bech32() == "erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2"
+        assert transaction.receiver.bech32() == "erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqtllllls002zgc"
+        assert transaction.chainID == "D"
+        assert transaction.nonce == 777
+        assert transaction.signature == b""
+        assert str(transaction.data) == "addNodes@e7beaa95b3877f47348df4dd1cb578a4f7cabf7a20bfeefe5cdd263878ff132b765e04fef6f40c93512b666c47ed7719b8902f6c922c04247989b7137e837cc81a62e54712471c97a2ddab75aa9c2f58f813ed4c0fa722bde0ab718bff382208@81109fa1c8d3dc7b6c2d6e65206cc0bc1a83c9b2d1eb91a601d66ad32def430827d5eb52917bd2b0d04ce195738db216"
