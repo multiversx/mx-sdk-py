61ef8bb02b93a22ef025a3f46ed96e6581f9c047
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-06-15 15:33:47 +0300

    refactoring

--- core/multiversx_sdk_core/transaction_builders/delegation_factory.py
+++ core/multiversx_sdk_core/transaction_builders/delegation_factory.py
@@ -1,9 +1,9 @@
-from typing import Any, List, Optional, Protocol
+from typing import Any, List, Optional, Protocol, Tuple
 
 from multiversx_sdk_core import TransactionPayload
-from multiversx_sdk_core.address import Address
 from multiversx_sdk_core.constants import (ARGS_SEPARATOR,
-                                           DELEGATION_MANAGER_SC_ADDRESS)
+                                           TRANSACTION_OPTIONS_DEFAULT,
+                                           TRANSACTION_VERSION_DEFAULT)
 from multiversx_sdk_core.errors import ErrListsLengthDoNotMatch
 from multiversx_sdk_core.interfaces import (IAddress, IChainID, IGasLimit,
                                             IGasPrice, INonce, ISignature,
@@ -19,29 +19,6 @@ class IConfig(Protocol):
     min_gas_price: IGasPrice
     min_gas_limit: IGasLimit
     gas_limit_per_byte: IGasLimit
-
-
-class IBaseArgs(Protocol):
-    transaction_nonce: Optional[INonce]
-    value: Optional[ITransactionValue]
-    gas_price: Optional[IGasPrice]
-    gas_limit: Optional[IGasLimit]
-
-
-class ICreateNewDelegationContractArgs(IBaseArgs, Protocol):
-    sender: IAddress
-    receiver: IAddress = Address.from_bech32(DELEGATION_MANAGER_SC_ADDRESS)
-    total_delegation_cap: int
-    service_fee: int
-    data: Optional[ITransactionPayload]
-
-
-class IAddNodesArgs(IBaseArgs, Protocol):
-    sender: IAddress
-    delegation_contract: IAddress
-    public_keys: List[IValidatorPublicKey]
-    signed_messages: List[ISignature]
-    data: Optional[ITransactionPayload]
 
 
 class MetaChainSystemSCsCost:
@@ -63,77 +40,118 @@ class DelegationFactory:
     def __init__(self, config: IConfig) -> None:
         self.config = config
 
-    def create_new_delegation_contract(self, args: ICreateNewDelegationContractArgs) -> Transaction:
-        if not args.data:
-            args.data = self._prepare_data_for_create_new_delegation_contract(args)
-
-        if not args.gas_limit:
-            args.gas_limit = self._estimate_system_sc_call(args.data.length(), MetaChainSystemSCsCost.DELEGATION_MANAGER_OPS, 2)
+    def create_new_delegation_contract(self,
+                                       sender: IAddress,
+                                       receiver: IAddress,
+                                       total_delegation_cap: int,
+                                       service_fee: int,
+                                       value: ITransactionValue,
+                                       transaction_nonce: Optional[INonce] = None,
+                                       gas_price: Optional[IGasPrice] = None,
+                                       gas_limit: Optional[IGasLimit] = None) -> Transaction:
+        data = self._prepare_data_for_create_new_delegation_contract(total_delegation_cap, service_fee)
 
         transaction = self.create_transaction(
-            args.sender,
-            args.receiver,
-            args.gas_limit,
-            args.data,
-            args.gas_price,
-            args.transaction_nonce,
-            args.value
+            sender=sender,
+            receiver=receiver,
+            data=data,
+            execution_gas_limit=MetaChainSystemSCsCost.DELEGATION_MANAGER_OPS,
+            gas_limit_hint=gas_limit,
+            gas_price=gas_price,
+            nonce=transaction_nonce,
+            value=value
         )
 
         return transaction
 
-    def _prepare_data_for_create_new_delegation_contract(self, args: ICreateNewDelegationContractArgs) -> TransactionPayload:
+    def _prepare_data_for_create_new_delegation_contract(self, total_delegation_cap: int, service_fee: int) -> TransactionPayload:
         function = "createNewDelegationContract"
-        args_list: List[Any] = [args.total_delegation_cap, args.service_fee]
+        args_list: List[Any] = [total_delegation_cap, service_fee]
         data = ARGS_SEPARATOR.join([function, args_to_string(args_list)])
         return TransactionPayload.from_str(data)
 
-    def add_nodes(self, args: IAddNodesArgs):
-        num_of_nodes = len(args.public_keys)
-
-        if not args.data:
-            args.data = self._prepare_data_for_add_nodes(args)
-
-        if not args.gas_limit:
-            args.gas_limit = self._estimate_system_sc_call(args.data.length(), MetaChainSystemSCsCost.DELEGATION_OPS, num_of_nodes + 1)
+    def add_nodes(self,
+                  sender: IAddress,
+                  delegation_contract: IAddress,
+                  public_keys: List[IValidatorPublicKey],
+                  signed_messages: List[ISignature],
+                  value: ITransactionValue,
+                  transaction_nonce: Optional[INonce] = None,
+                  gas_price: Optional[IGasPrice] = None,
+                  gas_limit: Optional[IGasLimit] = None) -> Transaction:
+        data = self._prepare_data_for_add_nodes(public_keys, signed_messages)
 
         transaction = self.create_transaction(
-            sender=args.sender,
-            receiver=args.delegation_contract,
-            gas_limit=args.gas_limit,
-            data=args.data,
-            gas_price=args.gas_price,
-            nonce=args.transaction_nonce,
-            value=args.value
+            sender=sender,
+            receiver=delegation_contract,
+            data=data,
+            execution_gas_limit=MetaChainSystemSCsCost.DELEGATION_OPS,
+            gas_limit_hint=gas_limit,
+            gas_price=gas_price,
+            nonce=transaction_nonce,
+            value=value
+        )
+        return transaction
+
+    def _prepare_data_for_add_nodes(self, public_keys: List[IValidatorPublicKey], signed_messages: List[ISignature]) -> TransactionPayload:
+        if len(public_keys) != len(signed_messages):
+            raise ErrListsLengthDoNotMatch("The number of public keys should match the number of signed messages")
+
+        add_nodes_data = "addNodes"
+        for i in range(len(public_keys)):
+            add_nodes_data += f"@{public_keys[i].hex()}@{signed_messages[i].hex()}"
+
+        return TransactionPayload.from_str(add_nodes_data)
+
+    def remove_nodes(self,
+                     sender: IAddress,
+                     delegation_contract: IAddress,
+                     bls_keys: List[str],
+                     value: ITransactionValue,
+                     transaction_nonce: Optional[INonce] = None,
+                     gas_price: Optional[IGasPrice] = None,
+                     gas_limit: Optional[IGasLimit] = None):
+        parsed_keys, _ = self._parse_keys(bls_keys)
+        data = TransactionPayload.from_str("removeNodes" + parsed_keys)
+
+        transaction = self.create_transaction(
+            sender=sender,
+            receiver=delegation_contract,
+            data=data,
+            execution_gas_limit=MetaChainSystemSCsCost.DELEGATION_OPS,
+            gas_limit_hint=gas_limit,
+            gas_price=gas_price,
+            nonce=transaction_nonce,
+            value=value
         )
 
         return transaction
 
-    def _prepare_data_for_add_nodes(self, args: IAddNodesArgs) -> TransactionPayload:
-        if len(args.public_keys) != len(args.signed_messages):
-            raise ErrListsLengthDoNotMatch("The number of public keys should match the number of signed messages")
+    def _compute_gas_limit(self, payload: ITransactionPayload, execution_gas: IGasLimit) -> IGasLimit:
+        data_movement_gas = self.config.min_gas_limit + self.config.gas_limit_per_byte * payload.length()
+        return data_movement_gas + execution_gas
 
-        add_nodes_data = "addNodes"
-        for i in range(len(args.public_keys)):
-            add_nodes_data += f"@{args.public_keys[i].hex()}@{args.signed_messages[i].hex()}"
-
-        return TransactionPayload.from_str(add_nodes_data)
-
-    def _estimate_system_sc_call(self, data_length: int, base_cost: int, factor: int = 1) -> int:
-        gas_limit = self.config.min_gas_limit + data_length * self.config.gas_limit_per_byte
-        gas_limit += factor * base_cost
-        return gas_limit
+    def _parse_keys(self, keys: List[str]) -> Tuple[str, int]:
+        parsed_keys = ''
+        for key in keys:
+            parsed_keys += '@' + key
+        return parsed_keys, len(keys)
 
     def create_transaction(
             self,
             sender: IAddress,
             receiver: IAddress,
-            gas_limit: IGasLimit,
             data: ITransactionPayload,
+            execution_gas_limit: IGasLimit,
+            gas_limit_hint: Optional[IGasLimit],
             gas_price: Optional[IGasPrice],
             nonce: Optional[INonce],
             value: Optional[ITransactionValue]
     ) -> Transaction:
+        gas_limit = gas_limit_hint or self._compute_gas_limit(data, execution_gas_limit)
+        version = TRANSACTION_VERSION_DEFAULT
+        options = TRANSACTION_OPTIONS_DEFAULT
+
         return Transaction(
             chain_id=self.config.chain_id,
             sender=sender,
@@ -142,5 +160,7 @@ class DelegationFactory:
             gas_price=gas_price,
             value=value,
             nonce=nonce,
-            data=data
+            data=data,
+            version=version,
+            options=options
         )
--- core/multiversx_sdk_core/transaction_builders/delegation_factory_test.py
+++ core/multiversx_sdk_core/transaction_builders/delegation_factory_test.py
@@ -1,11 +1,13 @@
+from typing import List
+
 from multiversx_sdk_wallet import ValidatorSecretKey, ValidatorSigner
 
 from multiversx_sdk_core.address import Address
 from multiversx_sdk_core.constants import DELEGATION_MANAGER_SC_ADDRESS
+from multiversx_sdk_core.interfaces import IValidatorPublicKey
 from multiversx_sdk_core.messages import ArbitraryMessage
 from multiversx_sdk_core.transaction_builders.delegation_factory import (
-    DelegationFactory, IAddNodesArgs, ICreateNewDelegationContractArgs,
-    MetaChainSystemSCsCost)
+    DelegationFactory, MetaChainSystemSCsCost)
 
 
 class Config:
@@ -16,56 +18,32 @@ class Config:
         self.gas_limit_per_byte = 1500
 
 
-class NewDelegationContractArgs(ICreateNewDelegationContractArgs):
-    def __init__(self) -> None:
-        self.sender = Address.from_bech32("erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2")
-        self.receiver = Address.from_bech32(DELEGATION_MANAGER_SC_ADDRESS)
-        self.transaction_nonce = 777
-        self.value = 1250000000000000000000
-        self.total_delegation_cap = 5000000000000000000000
-        self.service_fee = 10
-        self.data = None
-        self.gas_limit = None
-        self.gas_price = None
-
-
-class AddNodesArgs(IAddNodesArgs):
-    def __init__(self) -> None:
-        self.sender = Address.from_bech32("erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2")
-        self.delegation_contract = Address.from_bech32("erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqtllllls002zgc")
-
-        validator_secret_key = ValidatorSecretKey.from_string("7cff99bd671502db7d15bc8abc0c9a804fb925406fbdd50f1e4c17a4cd774247")
-        validator_signer = ValidatorSigner(validator_secret_key)
-
-        message = ArbitraryMessage(bytes.fromhex(self.delegation_contract.hex()))
-        signed_message = validator_signer.sign(message)
-        public_key = validator_secret_key.generate_public_key()
-
-        self.public_keys = [public_key]
-        self.signed_messages = [signed_message]
-
-        self.transaction_nonce = 777
-        self.data = None
-        self.gas_limit = None
-        self.gas_price = None
-        self.value = None
-
-
 class TestDelagationFactory:
     config = Config()
     factory = DelegationFactory(config)
 
     def test_compute_data_field_for_new_delegation_contract(self):
-        data = self.factory._prepare_data_for_create_new_delegation_contract(NewDelegationContractArgs())
+        data = self.factory._prepare_data_for_create_new_delegation_contract(total_delegation_cap=5000000000000000000000,
+                                                                             service_fee=10)
         assert str(data) == "createNewDelegationContract@010f0cf064dd59200000@0a"
 
     def test_compute_gas_limit_for_new_delegation_contract(self):
-        data = self.factory._prepare_data_for_create_new_delegation_contract(NewDelegationContractArgs())
-        gas_limit = self.factory._estimate_system_sc_call(data.length(), MetaChainSystemSCsCost.DELEGATION_MANAGER_OPS, 2)
-        assert gas_limit == 100126500
+        data = self.factory._prepare_data_for_create_new_delegation_contract(total_delegation_cap=5000000000000000000000,
+                                                                             service_fee=10)
+        gas_limit = self.factory._compute_gas_limit(data, MetaChainSystemSCsCost.DELEGATION_MANAGER_OPS)
+        assert gas_limit == 50126500
 
     def test_create_new_delegation_contract(self):
-        transaction = self.factory.create_new_delegation_contract(NewDelegationContractArgs())
+        transaction = self.factory.create_new_delegation_contract(
+            sender=Address.from_bech32("erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2"),
+            receiver=Address.from_bech32(DELEGATION_MANAGER_SC_ADDRESS),
+            transaction_nonce=777,
+            value=1250000000000000000000,
+            total_delegation_cap=5000000000000000000000,
+            service_fee=10,
+            gas_limit=None,
+            gas_price=None,
+        )
 
         assert transaction.sender.bech32() == "erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2"
         assert transaction.receiver.bech32() == DELEGATION_MANAGER_SC_ADDRESS
@@ -75,11 +53,50 @@ class TestDelagationFactory:
         assert str(transaction.data) == "createNewDelegationContract@010f0cf064dd59200000@0a"
 
     def test_compute_data_field_for_add_nodes(self):
-        data = self.factory._prepare_data_for_add_nodes(AddNodesArgs())
+        delegation_contract = Address.from_bech32("erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqtllllls002zgc")
+
+        validator_secret_key = ValidatorSecretKey.from_string("7cff99bd671502db7d15bc8abc0c9a804fb925406fbdd50f1e4c17a4cd774247")
+        validator_signer = ValidatorSigner(validator_secret_key)
+
+        message = ArbitraryMessage(bytes.fromhex(delegation_contract.hex()))
+        signed_message = validator_signer.sign(message)
+        public_key = validator_secret_key.generate_public_key()
+
+        public_keys = [public_key]
+        signed_messages = [signed_message]
+
+        data = self.factory._prepare_data_for_add_nodes(public_keys, signed_messages)
         assert str(data) == "addNodes@e7beaa95b3877f47348df4dd1cb578a4f7cabf7a20bfeefe5cdd263878ff132b765e04fef6f40c93512b666c47ed7719b8902f6c922c04247989b7137e837cc81a62e54712471c97a2ddab75aa9c2f58f813ed4c0fa722bde0ab718bff382208@81109fa1c8d3dc7b6c2d6e65206cc0bc1a83c9b2d1eb91a601d66ad32def430827d5eb52917bd2b0d04ce195738db216"
 
     def test_add_nodes(self):
-        transaction = self.factory.add_nodes(AddNodesArgs())
+        sender = Address.from_bech32("erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2")
+        delegation_contract = Address.from_bech32("erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqtllllls002zgc")
+
+        validator_secret_key = ValidatorSecretKey.from_string("7cff99bd671502db7d15bc8abc0c9a804fb925406fbdd50f1e4c17a4cd774247")
+        validator_signer = ValidatorSigner(validator_secret_key)
+
+        message = ArbitraryMessage(bytes.fromhex(delegation_contract.hex()))
+        signed_message = validator_signer.sign(message)
+        public_key = validator_secret_key.generate_public_key()
+
+        public_keys: List[IValidatorPublicKey] = [public_key]
+        signed_messages = [signed_message]
+
+        transaction_nonce = 777
+        gas_limit = None
+        gas_price = None
+        value = None
+
+        transaction = self.factory.add_nodes(
+            sender=sender,
+            delegation_contract=delegation_contract,
+            public_keys=public_keys,
+            signed_messages=signed_messages,
+            value=value,
+            transaction_nonce=transaction_nonce,
+            gas_price=gas_price,
+            gas_limit=gas_limit
+        )
 
         assert transaction.sender.bech32() == "erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2"
         assert transaction.receiver.bech32() == "erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqtllllls002zgc"
