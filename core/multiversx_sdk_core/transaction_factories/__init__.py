9907e022c7a707a3dc89e7adc70e7c08ab4d28fc
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2023-06-19 17:46:34 +0300

    Export modules.

--- core/multiversx_sdk_core/transaction_factories/__init__.py
+++ core/multiversx_sdk_core/transaction_factories/__init__.py
@@ -1,6 +1,9 @@
 from multiversx_sdk_core.transaction_factories.delegation_factory import \
     DelegationFactory
+from multiversx_sdk_core.transaction_factories.token_operations_factory import \
+    TokenOperationsFactory
 
 __all__ = [
-    "DelegationFactory"
+    "DelegationFactory",
+    "TokenOperationsFactory"
 ]
