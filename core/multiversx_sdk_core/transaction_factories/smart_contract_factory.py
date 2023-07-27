41942709b30586a4f65cfed5c58ed251f4e8f269
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-07-27 13:12:39 +0300

    smart contract factory rename & refactor

--- core/multiversx_sdk_core/transaction_factories/smart_contract_factory.py
+++ core/multiversx_sdk_core/transaction_factories/smart_contract_factory.py
@@ -1,18 +1,14 @@
 from pathlib import Path
-from typing import Any, List, Optional, Protocol
+from typing import Any, List, Protocol, Union
 
 from multiversx_sdk_core.address import Address
 from multiversx_sdk_core.code_metadata import CodeMetadata
 from multiversx_sdk_core.constants import (ARGS_SEPARATOR,
                                            CONTRACT_DEPLOY_ADDRESS,
-                                           TRANSACTION_OPTIONS_DEFAULT,
-                                           TRANSACTION_VERSION_DEFAULT,
                                            VM_TYPE_WASM_VM)
-from multiversx_sdk_core.interfaces import (IAddress, IChainID, IGasLimit,
-                                            IGasPrice, INonce,
-                                            ITransactionValue)
+from multiversx_sdk_core.interfaces import IAddress, IChainID, IGasLimit
 from multiversx_sdk_core.serializer import arg_to_string, args_to_strings
-from multiversx_sdk_core.transaction import Transaction
+from multiversx_sdk_core.transaction_intent import TransactionIntent
 from multiversx_sdk_core.transaction_payload import TransactionPayload
 
 
@@ -24,20 +20,18 @@ class SmartContractFactory:
     def __init__(self, config: IConfig) -> None:
         self.config = config
 
-    def create_transaction_for_deploy(self,
-                                      deployer: IAddress,
-                                      bytecode_path: Path,
-                                      gas_limit: IGasLimit,
-                                      arguments: List[Any] = [],
-                                      nonce: Optional[INonce] = None,
-                                      value: Optional[ITransactionValue] = None,
-                                      guardian: Optional[IAddress] = None,
-                                      gas_price: Optional[IGasPrice] = None,
-                                      is_upgradeable: bool = True,
-                                      is_readable: bool = True,
-                                      is_payable: bool = True,
-                                      is_payable_by_sc: bool = True) -> Transaction:
-        bytecode = bytecode_path.read_bytes()
+    def create_transaction_intent_for_deploy(self,
+                                             sender: IAddress,
+                                             bytecode: Union[Path, bytes],
+                                             gas_limit: int,
+                                             arguments: List[Any] = [],
+                                             is_upgradeable: bool = True,
+                                             is_readable: bool = True,
+                                             is_payable: bool = False,
+                                             is_payable_by_sc: bool = True) -> TransactionIntent:
+        if isinstance(bytecode, Path):
+            bytecode = bytecode.read_bytes()
+
         metadata = CodeMetadata(is_upgradeable, is_readable, is_payable, is_payable_by_sc)
 
         parts = [
@@ -48,38 +42,28 @@ class SmartContractFactory:
 
         parts += args_to_strings(arguments)
 
-        transaction = self.create_transaction(
-            sender=deployer,
+        transaction = self._create_transaction_intent(
+            sender=sender,
             receiver=Address.from_bech32(CONTRACT_DEPLOY_ADDRESS),
             data_parts=parts,
-            gas_limit=gas_limit,
-            gas_price=gas_price,
-            nonce=nonce,
-            value=value,
-            guardian=guardian
+            gas_limit=gas_limit
         )
 
         return transaction
 
-    def create_transaction_for_execute(self,
-                                       sender: IAddress,
-                                       contract_address: IAddress,
-                                       function: str,
-                                       gas_limit: IGasLimit,
-                                       arguments: List[Any] = [],
-                                       nonce: Optional[INonce] = None,
-                                       guardian: Optional[IAddress] = None,
-                                       value: Optional[ITransactionValue] = None) -> Transaction:
+    def create_transaction_intent_for_execute(self,
+                                              sender: IAddress,
+                                              contract_address: IAddress,
+                                              function: str,
+                                              gas_limit: int,
+                                              arguments: List[Any] = []) -> TransactionIntent:
         parts = [function] + args_to_strings(arguments)
 
-        transaction = self.create_transaction(
+        transaction = self._create_transaction_intent(
             sender=sender,
             receiver=contract_address,
             data_parts=parts,
-            gas_limit=gas_limit,
-            nonce=nonce,
-            guardian=guardian,
-            value=value
+            gas_limit=gas_limit
         )
 
         return transaction
@@ -87,17 +71,17 @@ class SmartContractFactory:
     def create_transaction_for_upgrade(self,
                                        sender: IAddress,
                                        contract: IAddress,
-                                       bytecode_path: Path,
-                                       gas_limit: IGasLimit,
+                                       bytecode: Union[Path, bytes],
+                                       gas_limit: int,
                                        arguments: List[Any] = [],
-                                       nonce: Optional[INonce] = None,
-                                       guardian: Optional[IAddress] = None,
                                        is_upgradeable: bool = True,
                                        is_readable: bool = True,
-                                       is_payable: bool = True,
+                                       is_payable: bool = False,
                                        is_payable_by_sc: bool = True
-                                       ) -> Transaction:
-        bytecode = bytecode_path.read_bytes()
+                                       ) -> TransactionIntent:
+        if isinstance(bytecode, Path):
+            bytecode = bytecode.read_bytes()
+
         metadata = CodeMetadata(is_upgradeable, is_readable, is_payable, is_payable_by_sc)
 
         parts = [
@@ -108,44 +92,31 @@ class SmartContractFactory:
 
         parts += args_to_strings(arguments)
 
-        transaction = self.create_transaction(
+        transaction = self._create_transaction_intent(
             sender=sender,
             receiver=contract,
             data_parts=parts,
-            gas_limit=gas_limit,
-            nonce=nonce,
-            guardian=guardian
+            gas_limit=gas_limit
         )
 
         return transaction
 
-    def create_transaction(
+    def _create_transaction_intent(
             self,
             sender: IAddress,
             receiver: IAddress,
             data_parts: List[str],
-            gas_limit: IGasLimit,
-            nonce: Optional[INonce],
-            value: Optional[ITransactionValue] = None,
-            guardian: Optional[IAddress] = None,
-            gas_price: Optional[IGasPrice] = None) -> Transaction:
+            gas_limit: IGasLimit) -> TransactionIntent:
         data = self._build_transaction_payload(data_parts)
-        version = TRANSACTION_VERSION_DEFAULT
-        options = TRANSACTION_OPTIONS_DEFAULT
 
-        return Transaction(
-            chain_id=self.config.chain_id,
-            sender=sender,
-            receiver=receiver,
-            gas_limit=gas_limit,
-            gas_price=gas_price,
-            value=value,
-            nonce=nonce,
-            data=data,
-            version=version,
-            options=options,
-            guardian=guardian
-        )
+        transaction_intent = TransactionIntent()
+
+        transaction_intent.sender = sender.bech32()
+        transaction_intent.receiver = receiver.bech32()
+        transaction_intent.gas_limit = gas_limit
+        transaction_intent.data = str(data).encode()
+
+        return transaction_intent
 
     def _build_transaction_payload(self, parts: List[str]) -> TransactionPayload:
         data = ARGS_SEPARATOR.join(parts)
--- core/multiversx_sdk_core/transaction_factories/smart_contract_factory_test.py
+++ core/multiversx_sdk_core/transaction_factories/smart_contract_factory_test.py
@@ -14,62 +14,47 @@ class TestSmartContract:
 
     def test_smart_contract_deploy(self):
         sender = Address.from_bech32("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th")
-        guardian = Address.from_bech32("erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx")
-        nonce = 777
         contract = Path(__file__).parent.parent / "testutils" / "testdata" / "adder.wasm"
         gas_limit = 6000000
         args = [0]
 
-        transaction = self.factory.create_transaction_for_deploy(
-            deployer=sender,
-            nonce=nonce,
-            bytecode_path=contract,
+        transaction = self.factory.create_transaction_intent_for_deploy(
+            sender=sender,
+            bytecode=contract,
             gas_limit=gas_limit,
-            arguments=args,
-            guardian=guardian
+            arguments=args
         )
 
-        assert transaction.chainID == "D"
-        assert transaction.nonce == 777
-        assert transaction.sender.bech32() == "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"
-        assert transaction.receiver.bech32() == CONTRACT_DEPLOY_ADDRESS
-        assert transaction.guardian
-        assert transaction.guardian.bech32() == "erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx"
+        assert transaction.sender == "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"
+        assert transaction.receiver == CONTRACT_DEPLOY_ADDRESS
         assert transaction.gas_limit == 6000000
-        assert transaction.signature == b''
+        assert transaction.data
+        assert transaction.value == "0"
 
     def test_smart_contract_call(self):
         sender = Address.from_bech32("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th")
-        guardian = Address.from_bech32("erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx")
-        nonce = 777
         contract = Address.from_bech32("erd1qqqqqqqqqqqqqpgqhy6nl6zq07rnzry8uyh6rtyq0uzgtk3e69fqgtz9l4")
         function = "add"
         gas_limit = 6000000
         args = [7]
 
-        transaction = self.factory.create_transaction_for_execute(
+        transaction = self.factory.create_transaction_intent_for_execute(
             sender=sender,
             contract_address=contract,
             function=function,
             gas_limit=gas_limit,
-            nonce=nonce,
-            arguments=args,
-            guardian=guardian
+            arguments=args
         )
 
-        assert transaction.chainID == "D"
-        assert transaction.nonce == 777
-        assert transaction.sender.bech32() == "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"
-        assert transaction.receiver.bech32() == contract.bech32()
-        assert transaction.guardian
-        assert transaction.guardian.bech32() == "erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx"
-        assert str(transaction.data) == "add@07"
-        assert transaction.value == 0
+        assert transaction.sender == "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"
+        assert transaction.receiver == contract.bech32()
+        assert transaction.gas_limit == 6000000
+        assert transaction.data
+        assert transaction.data.decode() == "add@07"
+        assert transaction.value == "0"
 
     def test_smart_contract_upgrade(self):
         sender = Address.from_bech32("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th")
-        guardian = Address.from_bech32("erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx")
-        nonce = 777
         contract_address = Address.from_bech32("erd1qqqqqqqqqqqqqpgqhy6nl6zq07rnzry8uyh6rtyq0uzgtk3e69fqgtz9l4")
         contract = Path(__file__).parent.parent / "testutils" / "testdata" / "adder.wasm"
         gas_limit = 6000000
@@ -78,19 +63,14 @@ class TestSmartContract:
         transaction = self.factory.create_transaction_for_upgrade(
             sender=sender,
             contract=contract_address,
-            bytecode_path=contract,
+            bytecode=contract,
             gas_limit=gas_limit,
-            arguments=args,
-            nonce=nonce,
-            guardian=guardian
+            arguments=args
         )
 
-        assert transaction.chainID == "D"
-        assert transaction.nonce == 777
-        assert transaction.sender.bech32() == "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"
-        assert transaction.receiver.bech32() == "erd1qqqqqqqqqqqqqpgqhy6nl6zq07rnzry8uyh6rtyq0uzgtk3e69fqgtz9l4"
-        assert transaction.guardian
-        assert transaction.guardian.bech32() == "erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx"
+        assert transaction.sender == "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"
+        assert transaction.receiver == "erd1qqqqqqqqqqqqqpgqhy6nl6zq07rnzry8uyh6rtyq0uzgtk3e69fqgtz9l4"
         assert transaction.gas_limit == 6000000
-        assert transaction.signature == b''
-        assert str(transaction.data).startswith("upgradeContract@")
+        assert transaction.data
+        assert transaction.data.decode().startswith("upgradeContract@")
+        assert transaction.value == "0"
