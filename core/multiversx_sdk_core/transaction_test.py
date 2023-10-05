2ba4ad97eca96d867cad4374d5ff3d4b99ac5df8
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-10-05 20:08:37 +0300

    fix unit tests for the Transaction class

--- core/multiversx_sdk_core/transaction_test.py
+++ core/multiversx_sdk_core/transaction_test.py
@@ -1,63 +1,96 @@
-from multiversx_sdk_core.address import Address
+import pytest
+
 from multiversx_sdk_core.testutils.wallets import load_wallets
-from multiversx_sdk_core.token_payment import TokenPayment
-from multiversx_sdk_core.transaction import Transaction
-from multiversx_sdk_core.transaction_payload import TransactionPayload
+from multiversx_sdk_core.transaction import Transaction, TransactionComputer
 
 
 class TestTransaction:
     wallets = load_wallets()
     alice = wallets["alice"]
     carol = wallets["carol"]
+    transaction_computer = TransactionComputer()
 
     def test_serialize_for_signing(self):
-        sender = Address.from_bech32("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th")
-        receiver = Address.from_bech32("erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx")
+        sender = "erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th"
+        receiver = "erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx"
 
         transaction = Transaction(
             nonce=89,
             sender=sender,
             receiver=receiver,
-            value=0,
+            amount=0,
             gas_limit=50000,
             gas_price=1000000000,
             chain_id="D",
             version=1
         )
-
-        assert transaction.serialize_for_signing().decode() == r"""{"nonce":89,"value":"0","receiver":"erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx","sender":"erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th","gasPrice":1000000000,"gasLimit":50000,"chainID":"D","version":1}"""
+        serialized_tx = self.transaction_computer.compute_bytes_for_signing(transaction)
+        assert serialized_tx.decode() == r"""{"nonce":89,"value":"0","receiver":"erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx","sender":"erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th","gasPrice":1000000000,"gasLimit":50000,"chainID":"D","version":1}"""
 
         transaction = Transaction(
             nonce=90,
             sender=sender,
             receiver=receiver,
-            value=TokenPayment.egld_from_amount("1.0"),
-            data=TransactionPayload.from_str("hello"),
+            amount=1000000000000000000,
+            data=b"hello",
             gas_limit=70000,
             gas_price=1000000000,
             chain_id="D",
             version=1
         )
-
-        assert transaction.serialize_for_signing().decode() == r"""{"nonce":90,"value":"1000000000000000000","receiver":"erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx","sender":"erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th","gasPrice":1000000000,"gasLimit":70000,"data":"aGVsbG8=","chainID":"D","version":1}"""
+        serialized_tx = self.transaction_computer.compute_bytes_for_signing(transaction)
+        assert serialized_tx.decode() == r"""{"nonce":90,"value":"1000000000000000000","receiver":"erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx","sender":"erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th","gasPrice":1000000000,"gasLimit":70000,"data":"aGVsbG8=","chainID":"D","version":1}"""
 
     def test_with_usernames(self):
         transaction = Transaction(
             chain_id="T",
-            sender=Address.from_bech32(self.carol.label),
-            receiver=Address.from_bech32(self.alice.label),
+            sender=self.carol.label,
+            receiver=self.alice.label,
             nonce=204,
             gas_limit=50000,
             sender_username="carol",
             receiver_username="alice",
-            value=1000000000000000000
+            amount=1000000000000000000
         )
         # needs to match the test from sdk-js-core
         transaction.version = 1
 
-        transaction.signature = self.carol.secret_key.sign(transaction.serialize_for_signing())
+        transaction.signature = self.carol.secret_key.sign(self.transaction_computer.compute_bytes_for_signing(transaction))
         assert transaction.signature.hex() == "5966dd6b98fc5ecbcd203fa38fac7059ba5c17683099071883b0ad6697386769321d851388a99cb8b81aab625aa2d7e13621432dbd8ab334c5891cd7c7755200"
 
+    def test_compute_transaction_hash(self):
+        transaction = Transaction(
+            sender="erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th",
+            receiver="erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th",
+            gas_limit=100000,
+            chain_id="D",
+            nonce=17243,
+            amount=1000000000000,
+            data=b"testtx",
+            version=2,
+            signature=bytes.fromhex("eaa9e4dfbd21695d9511e9754bde13e90c5cfb21748a339a79be11f744c71872e9fe8e73c6035c413f5f08eef09e5458e9ea6fc315ff4da0ab6d000b450b2a07")
+        )
+        tx_hash = self.transaction_computer.compute_transaction_hash(transaction)
+        assert tx_hash == "169b76b752b220a76a93aeebc462a1192db1dc2ec9d17e6b4d7b0dcc91792f03"
+
+    def test_compute_transaction_hash_with_usernames(self):
+        transaction = Transaction(
+            sender="erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th",
+            receiver="erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th",
+            gas_limit=100000,
+            chain_id="D",
+            nonce=17244,
+            amount=1000000000000,
+            data=b"testtx",
+            version=2,
+            sender_username="alice",
+            receiver_username="alice",
+            signature=bytes.fromhex("807bcd7de5553ea6dfc57c0510e84d46813c5963d90fec50991c500091408fcf6216dca48dae16a579a1611ed8b2834bae8bd0027dc17eb557963f7151b82c07")
+        )
+        tx_hash = self.transaction_computer.compute_transaction_hash(transaction)
+        assert tx_hash == "41b5acf7ebaf4a9165a64206b6ebc02021b3adda55ffb2a2698aac2e7004dc29"
+
+    @pytest.mark.skip("not sure what will happend with `from_dictionary()`")
     def test_tx_from_dictionary(self):
         tx_as_dict = {
             "nonce": 7,
