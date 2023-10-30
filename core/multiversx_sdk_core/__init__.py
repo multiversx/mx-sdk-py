262a4749cbc870b257c5e3e6ec4aebe46e278240
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-10-30 12:44:50 +0200

    update the Message class according to specs

--- core/multiversx_sdk_core/__init__.py
+++ core/multiversx_sdk_core/__init__.py
@@ -4,18 +4,16 @@ from multiversx_sdk_core.address import (Address, AddressConverter,
 from multiversx_sdk_core.code_metadata import CodeMetadata
 from multiversx_sdk_core.contract_query import ContractQuery
 from multiversx_sdk_core.contract_query_builder import ContractQueryBuilder
-from multiversx_sdk_core.tokens import (Token, TokenComputer,
-                                        TokenTransfer)
-from multiversx_sdk_core.messages import ArbitraryMessage, MessageV1
+from multiversx_sdk_core.messages import Message
 from multiversx_sdk_core.token_payment import TokenPayment
+from multiversx_sdk_core.tokens import Token, TokenComputer, TokenTransfer
 from multiversx_sdk_core.transaction import Transaction, TransactionComputer
 from multiversx_sdk_core.transaction_payload import TransactionPayload
 
 __all__ = [
     "AccountNonceHolder", "Address", "AddressConverter", "AddressFactory",
     "Transaction", "TransactionPayload", "TransactionComputer",
-    "ArbitraryMessage", "MessageV1",
-    "CodeMetadata", "TokenPayment",
+    "Message", "CodeMetadata", "TokenPayment",
     "ContractQuery", "ContractQueryBuilder",
     "Token", "TokenComputer", "TokenTransfer"
 ]
--- core/multiversx_sdk_core/messages.py
+++ core/multiversx_sdk_core/messages.py
@@ -1,49 +1,26 @@
 from Cryptodome.Hash import keccak
 
-from multiversx_sdk_core.interfaces import ISignature
+
+class Message:
+    def __init__(self, data: bytes, signature: bytes = b"") -> None:
+        self.data = data
+        self.signature = signature
 
 
-class MessageV1:
+class MessageComputer:
     """
     Also see: 
      - https://github.com/multiversx/mx-sdk-js-core/blob/v11.2.0/src/signableMessage.ts
      - https://eips.ethereum.org/EIPS/eip-712 (in the past, it served as a basis for the implementation)
     """
 
-    def __init__(self, data: bytes) -> None:
-        self.data: bytes = data
-        self.signature: ISignature = bytes()
+    def __init__(self) -> None:
+        pass
 
-    @classmethod
-    def from_string(cls, data: str) -> 'MessageV1':
-        return MessageV1(data.encode())
-
-    def serialize_for_signing(self) -> bytes:
+    def compute_bytes_for_signing(self, message: Message) -> bytes:
         PREFIX = bytes.fromhex("17456c726f6e64205369676e6564204d6573736167653a0a")
-        size = str(len(self.data)).encode()
-        content = PREFIX + size + self.data
+        size = str(len(message.data)).encode()
+        content = PREFIX + size + message.data
         content_hash = keccak.new(digest_bits=256).update(content).digest()
 
         return content_hash
-
-
-class ArbitraryMessage:
-    """
-    IMPORTANT: this should be rarely used in practice. 
-
-    For signing messages with a regular user wallet, use MessageV1, instead.
-
-    This type of messages allows one to sign arbitrary data.
-    It's sometimes used to sign data with a validator wallet (e.g. for composing the "data" field of staking transactions).
-    """
-
-    def __init__(self, data: bytes) -> None:
-        self.data: bytes = data
-        self.signature: ISignature = bytes()
-
-    @classmethod
-    def from_string(cls, data: str) -> 'ArbitraryMessage':
-        return cls(data.encode())
-
-    def serialize_for_signing(self) -> bytes:
-        return self.data
