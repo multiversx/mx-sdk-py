baeb9a5bd4256343e0201eae700a12cba588e6dc
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-10-30 13:09:06 +0200

    rename files

--- core/multiversx_sdk_core/__init__.py
+++ core/multiversx_sdk_core/__init__.py
@@ -4,7 +4,7 @@ from multiversx_sdk_core.address import (Address, AddressConverter,
 from multiversx_sdk_core.code_metadata import CodeMetadata
 from multiversx_sdk_core.contract_query import ContractQuery
 from multiversx_sdk_core.contract_query_builder import ContractQueryBuilder
-from multiversx_sdk_core.messages import Message
+from multiversx_sdk_core.message import Message
 from multiversx_sdk_core.token_payment import TokenPayment
 from multiversx_sdk_core.tokens import Token, TokenComputer, TokenTransfer
 from multiversx_sdk_core.transaction import Transaction, TransactionComputer
