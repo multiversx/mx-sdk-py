cbda546d0800fbb3f30722eea678e57975aa797b
Alexandru Popenta <alexandru.p9913@gmail.com>
2023-11-03 17:09:08 +0200

    fix api provider tests

--- network_providers/multiversx_sdk_network_providers/api_network_provider_test.py
+++ network_providers/multiversx_sdk_network_providers/api_network_provider_test.py
@@ -22,7 +22,7 @@ class Pagination(IPagination):
 
 
 class TestApi:
-    api = ApiNetworkProvider('https://devnet-old-api.multiversx.com')
+    api = ApiNetworkProvider('https://devnet-api.multiversx.com')
 
     def test_get_network_stake_statistic(self):
         result = self.api.get_network_stake_statistics()
@@ -35,12 +35,12 @@ class TestApi:
         result = self.api.get_network_general_statistics()
 
         assert result.shards == 3
-        assert result.rounds_per_epoch == 1200
+        assert result.rounds_per_epoch == 2400
         assert result.refresh_rate == 6000
-        assert result.epoch >= 2864
-        assert result.rounds_passed >= 0
-        assert result.transactions >= 4330650
-        assert result.accounts >= 92270
+        assert result.epoch
+        assert result.rounds_passed
+        assert result.transactions
+        assert result.accounts
 
     def test_get_network_gas_configs(self):
         result = self.api.get_network_gas_configs()
@@ -51,10 +51,10 @@ class TestApi:
         assert meta_system_sc_cost["Stake"] == 5000000
 
     def test_get_account(self):
-        address = Address.new_from_bech32('erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7')
+        address = Address.new_from_bech32('erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl')
         result = self.api.get_account(address)
 
-        assert result.address.to_bech32() == 'erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7'
+        assert result.address.to_bech32() == 'erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl'
         assert result.username == ''
 
     def test_get_generic_with_bad_address(self):
@@ -63,44 +63,43 @@ class TestApi:
             self.api.do_get_generic(url)
 
     def test_get_fungible_token_of_account(self):
-        address = Address.new_from_bech32('erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7')
-        result = self.api.get_fungible_token_of_account(address, 'ABC-10df96')
-
-        assert result.identifier == 'ABC-10df96'
+        address = Address.new_from_bech32('erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl')
+        result = self.api.get_fungible_token_of_account(address, "TEST-ff155e")
+        assert result.identifier == "TEST-ff155e"
 
     def test_get_nonfungible_token_of_account(self):
-        address = Address.new_from_bech32('erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7')
-        result = self.api.get_nonfungible_token_of_account(address, 'ASDASD-510041', 2)
+        address = Address.new_from_bech32('erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl')
+        result = self.api.get_nonfungible_token_of_account(address, "NFTEST-ec88b8", 1)
 
         assert result.balance == 0
-        assert result.nonce == 2
-        assert result.collection == 'ASDASD-510041'
-        assert result.identifier == 'ASDASD-510041-02'
+        assert result.nonce == 1
+        assert result.collection == "NFTEST-ec88b8"
+        assert result.identifier == "NFTEST-ec88b8-01"
         assert result.type == 'NonFungibleESDT'
-        assert result.royalties == 75
+        assert result.royalties == 25
 
     def test_get_meta_esdt(self):
-        adr = Address.new_from_bech32('erd1dk5urklhptgjp69k684wzapjxdp40fu0a3jn39rtcc78wxhewkyscp53au')
-        result = self.api.get_nonfungible_token_of_account(adr, 'EGLDRIDEF-9cf6f6', 4)
+        adr = Address.new_from_bech32('erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl')
+        result = self.api.get_nonfungible_token_of_account(adr, 'METATEST-ebeb7e', 1)
 
         assert result.balance != 0
-        assert result.nonce == 4
-        assert result.identifier == 'EGLDRIDEF-9cf6f6-04'
-        assert result.decimals == 18
+        assert result.nonce == 1
+        assert result.identifier == 'METATEST-ebeb7e-01'
+        assert result.decimals == 6
 
     def test_get_transaction(self):
-        result = self.api.get_transaction('2cb813be9d5e5040abb2522da75fa5c8d94f72caa510ff51d7525659f398298b')
+        result = self.api.get_transaction('9d47c4b4669cbcaa26f5dec79902dd20e55a0aa5f4b92454a74e7dbd0183ad6c')
 
-        assert result.hash == '2cb813be9d5e5040abb2522da75fa5c8d94f72caa510ff51d7525659f398298b'
-        assert result.nonce == 828
+        assert result.hash == '9d47c4b4669cbcaa26f5dec79902dd20e55a0aa5f4b92454a74e7dbd0183ad6c'
+        assert result.nonce == 0
         assert result.is_completed
-        assert result.sender.to_bech32() == 'erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7'
-        assert result.receiver.to_bech32() == 'erd1c8tnzykaj7lhrd5cy6jap533afr4dqu7uqcdm6qv4wuwly9lcsqqm9ll4f'
-        assert result.value == '10000000000000000000'
+        assert result.sender.to_bech32() == 'erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2'
+        assert result.receiver.to_bech32() == 'erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl'
+        assert result.value == '5000000000000000000'
         assert str(result.status) == "success"
 
     def test_get_account_transactions(self):
-        address = Address.new_from_bech32('erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7')
+        address = Address.new_from_bech32('erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl')
         pagination = Pagination(0, 2)
 
         result = self.api.get_account_transactions(address, pagination)
@@ -108,95 +107,91 @@ class TestApi:
         assert len(result) == 2
 
     def test_get_trasactions(self):
-        hashes = ["19de07b20873c6b9c77d3666eab532329f16f55a62b9fc961e52e4e0d57835d6", "8e43a0fb73bb97d1cacbca7de6d90ffacd9b7e10773f4ba37c6b8adbad6461dc"]
+        hashes = ["9d47c4b4669cbcaa26f5dec79902dd20e55a0aa5f4b92454a74e7dbd0183ad6c", "6fe05e4ca01d42c96ae5182978a77fe49f26bcc14aac95ad4f19618173f86ddb"]
         result = self.api.get_bunch_of_transactions(hashes)
 
         assert len(result) == 2
-        assert result[0].status.is_failed()
+        assert result[0].status.is_successful()
         assert result[1].status.is_successful()
 
     def test_get_transaction_with_events(self):
-        transaction = self.api.get_transaction("c451566a6168e38d2980fcb83d4ea154f78d53f7abf3264dd51c2c7c585671aa")
+        transaction = self.api.get_transaction("6fe05e4ca01d42c96ae5182978a77fe49f26bcc14aac95ad4f19618173f86ddb")
         assert transaction.logs
         assert transaction.logs.events
         assert len(transaction.logs.events) == 2
-        assert len(transaction.logs.events[0].topics) == 4
-        assert transaction.logs.events[0].topics[0].hex() == "5745474c442d643763366262"
+        assert len(transaction.logs.events[0].topics) == 8
+        assert transaction.logs.events[0].topics[0].hex() == "544553542d666631353565"
         assert transaction.logs.events[0].topics[1].hex() == ""
-        assert transaction.logs.events[0].topics[2].hex() == "0de0b6b3a7640000"
-        assert transaction.logs.events[0].topics[3].hex() == "00000000000000000500e01285f90311fb5925a9623a1dc62eee41fa8c869a0d"
+        assert transaction.logs.events[0].topics[2].hex() == "63616e4368616e67654f776e6572"
 
     def test_get_transactions_in_mempool_for_account(self):
-        address = Address.new_from_bech32("erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7")
+        address = Address.new_from_bech32("erd18s6a06ktr2v6fgxv4ffhauxvptssnaqlds45qgsrucemlwc8rawq553rt2")
         result = self.api.get_transactions_in_mempool_for_account(address)
-
         assert len(result) == 0
 
     def test_get_sc_invoking_tx(self):
-        result = self.api.get_transaction('cd2da63a51fd422c8b69a1b5ebcb9edbbf0eb9750c3fe8e199d39ed5d82000e9')
+        result = self.api.get_transaction('6fe05e4ca01d42c96ae5182978a77fe49f26bcc14aac95ad4f19618173f86ddb')
 
         assert result.is_completed == True
         assert len(result.contract_results.items) > 0
-        assert result.data == 'issue@54455354@54455354@03e8@00@63616e4d696e74@74727565@63616e4275726e@74727565@63616e4368616e67654f776e6572@74727565@63616e55706772616465@74727565'
+        assert result.data == 'issue@54455354546f6b656e@54455354@016345785d8a0000@06@63616e4368616e67654f776e6572@74727565@63616e55706772616465@74727565@63616e4164645370656369616c526f6c6573@74727565'
 
     def test_get_transaction_status(self):
-        result = self.api.get_transaction_status('2cb813be9d5e5040abb2522da75fa5c8d94f72caa510ff51d7525659f398298b')
-
+        result = self.api.get_transaction_status('6fe05e4ca01d42c96ae5182978a77fe49f26bcc14aac95ad4f19618173f86ddb')
         assert result.status == 'success'
 
     def test_query_contract(self):
-        query = ContractQuery(Address.new_from_bech32('erd1qqqqqqqqqqqqqpgquykqja5c4v33zdmnwglj3jphqwrelzdn396qlc9g33'),
+        query = ContractQuery(Address.new_from_bech32('erd1qqqqqqqqqqqqqpgqqy34h7he2ya6qcagqre7ur7cc65vt0mxrc8qnudkr4'),
                               'getSum', 0, [])
         result = self.api.query_contract(query)
-
         assert len(result.return_data) == 1
 
     def test_get_definition_of_fungible_token(self):
-        result = self.api.get_definition_of_fungible_token('ABC-10df96')
+        result = self.api.get_definition_of_fungible_token('TEST-ff155e')
 
-        assert result.identifier == 'ABC-10df96'
-        assert result.owner.to_bech32() == 'erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7'
+        assert result.identifier == 'TEST-ff155e'
+        assert result.owner.to_bech32() == 'erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl'
         assert result.can_upgrade
         assert not result.can_freeze
-        assert result.decimals == 1
-        assert result.supply == 5
+        assert result.decimals == 6
+        assert result.supply == 100000000000
 
     def test_get_definition_of_token_collection(self):
-        result = self.api.get_definition_of_token_collection('ASDASD-510041')
+        result = self.api.get_definition_of_token_collection('NFTEST-ec88b8')
 
-        assert result.collection == 'ASDASD-510041'
-        assert result.owner.to_bech32() == 'erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7'
+        assert result.collection == 'NFTEST-ec88b8'
+        assert result.owner.to_bech32() == 'erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl'
         assert result.type == 'NonFungibleESDT'
         assert result.decimals == 0
-        assert result.can_freeze
+        assert not result.can_freeze
         assert not result.can_pause
 
     def test_get_non_fungible_token(self):
-        result = self.api.get_non_fungible_token('ASDASD-510041', 2)
+        result = self.api.get_non_fungible_token('NFTEST-ec88b8', 1)
 
         assert result.type == 'NonFungibleESDT'
-        assert result.nonce == 2
-        assert result.identifier == 'ASDASD-510041-02'
-        assert result.collection == 'ASDASD-510041'
-        assert result.creator.to_bech32() == 'erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7'
+        assert result.nonce == 1
+        assert result.identifier == 'NFTEST-ec88b8-01'
+        assert result.collection == 'NFTEST-ec88b8'
+        assert result.creator.to_bech32() == 'erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl'
         assert result.balance == 0
-        assert result.royalties != 0
-        assert result.timestamp != 0
+        assert result.royalties == 25
+        assert result.timestamp == 1699019354
 
     def test_send_transaction(self):
         transaction = Transaction(
-            sender="erd15x2panzqvfxul2lvstfrmdcl5t4frnsylfrhng8uunwdssxw4y9succ9sq",
-            receiver="erd1testnlersh4z0wsv8kjx39me4rmnvjkwu8dsaea7ukdvvc9z396qykv7z7",
+            sender="erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl",
+            receiver="erd1487vz5m4zpxjyqw4flwa3xhnkzg4yrr3mkzf5sf0zgt94hjprc8qazcccl",
             gas_limit=50000,
             chain_id="D",
-            amount=1,
-            nonce=42,
+            amount=5000000000000000000,
+            nonce=100,
             gas_price=1000000000,
-            version=1,
-            signature=bytes.fromhex("c8eb539e486db7d703d8c70cab3b7679113f77c4685d8fcc94db027ceacc6b8605115034355386dffd7aa12e63dbefa03251a2f1b1d971f52250187298d12900")
+            version=2,
+            signature=bytes.fromhex("faf50b8368cb2c20597dad671a14aa76d4c65937d6e522c64946f16ad6a250262463e444596fa7ee2af1273f6ad0329d43af48d1ae5f3b295bc8f48fdba41a05")
         )
         expected_hash = (
-            "6e2fa63ea02937f00d7549f3e4eb9af241e4ac13027aa65a5300816163626c01"
+            "fc914860c1d137ed8baa602e561381f97c7bad80d150c5bf90760d3cfd3a4cea"
         )
 
         assert self.api.send_transaction(transaction) == expected_hash
