65f4e7b4e5b173251cb8d9fda5e99d109f8354e0
Alexandru Popenta <alexandru.p9913@gmail.com>
2024-04-10 14:49:25 +0300

    export smart contract deploy transactions parser

--- multiversx_sdk/core/__init__.py
+++ multiversx_sdk/core/__init__.py
@@ -30,6 +30,8 @@ from multiversx_sdk.core.transactions_outcome_parsers.delegation_transactions_ou
 from multiversx_sdk.core.transactions_outcome_parsers.resources import (
     SmartContractResult, TransactionEvent, TransactionLogs, TransactionOutcome,
     find_events_by_identifier)
+from multiversx_sdk.core.transactions_outcome_parsers.smart_contract_transactions_outcome_parser import \
+    SmartContractTransactionsOutcomeParser
 from multiversx_sdk.core.transactions_outcome_parsers.token_management_transactions_outcome_parser import \
     TokenManagementTransactionsOutcomeParser
 
@@ -45,5 +47,5 @@ __all__ = [
     "RegisterAndSetAllRolesTokenType", "TransactionsFactoryConfig",
     "SmartContractTransactionsFactory", "TransferTransactionsFactory",
     "RelayedTransactionsFactory", "AccountTransactionsFactory", "DelegationTransactionsOutcomeParser",
-    "find_events_by_identifier"
+    "find_events_by_identifier", "SmartContractTransactionsOutcomeParser"
 ]
