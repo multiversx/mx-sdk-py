10a22f70af34a13d047c7c7db5a5d07662ffc8ba
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-05-16 14:59:00 +0300

    Add type formula parser.

---
+++ multiversx_sdk/abi/type_formula.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class TypeFormula:
+    def __init__(self, name: str, type_parameters: List['TypeFormula']) -> None:
+        self.name: str = name
+        self.type_parameters: List[TypeFormula] = type_parameters
+
+    def __str__(self) -> str:
+        if self.type_parameters:
+            type_parameters = ", ".join([str(type_parameter) for type_parameter in self.type_parameters])
+            return f"{self.name}<{type_parameters}>"
+        else:
+            return self.name
---
+++ multiversx_sdk/abi/type_formula_parser.py
@@ -0,0 +1,91 @@
+
+from typing import Any, List
+
+from multiversx_sdk.abi.type_formula import TypeFormula
+
+
+class TypeFormulaParser:
+    BEGIN_TYPE_PARAMETERS = "<"
+    END_TYPE_PARAMETERS = ">"
+    COMMA = ","
+    PUNCTUATION = [COMMA, BEGIN_TYPE_PARAMETERS, END_TYPE_PARAMETERS]
+
+    def parse_expression(self, expression: str) -> TypeFormula:
+        expression = expression.strip()
+
+        tokens = self._tokenize_expression(expression)
+        tokens = [token for token in tokens if token != TypeFormulaParser.COMMA]
+
+        stack: List[Any] = []
+
+        for token in tokens:
+            if token in TypeFormulaParser.PUNCTUATION:
+                if token == TypeFormulaParser.END_TYPE_PARAMETERS:
+                    type_parameters: List[TypeFormula] = []
+
+                    while True:
+                        if not stack:
+                            raise Exception("Badly specified type parameters.")
+
+                        if stack[-1] == TypeFormulaParser.BEGIN_TYPE_PARAMETERS:
+                            break
+
+                        item = stack.pop()
+                        if isinstance(item, TypeFormula):
+                            type_formula = item
+                        else:
+                            type_formula = TypeFormula(item, [])
+
+                        type_parameters.append(type_formula)
+
+                    stack.pop()  # pop "<" symbol
+                    type_name = stack.pop()
+                    type_formula = TypeFormula(type_name, list(reversed(type_parameters)))
+                    stack.append(type_formula)
+                elif token == TypeFormulaParser.BEGIN_TYPE_PARAMETERS:
+                    # The symbol is pushed as a simple string,
+                    # as it will never be interpreted, anyway.
+                    stack.append(token)
+                elif token == TypeFormulaParser.COMMA:
+                    # We simply ignore commas
+                    pass
+                else:
+                    raise Exception(f"Unexpected token (punctuation): {token}.")
+            else:
+                # It's a type name. We push it as a simple string.
+                stack.append(token)
+
+        if len(stack) != 1:
+            raise Exception(f"Unexpected stack length at end of parsing: {len(stack)}.")
+        if stack[0] in TypeFormulaParser.PUNCTUATION:
+            raise Exception(f"Unexpected root element.")
+
+        if isinstance(stack[0], str):
+            # Expression contained a simple, non-generic type
+            return TypeFormula(stack[0], [])
+        elif isinstance(stack[0], TypeFormula):
+            return stack[0]
+        else:
+            raise Exception(f"Unexpected item on stack: {stack[0]}")
+
+    def _tokenize_expression(self, expression: str) -> List[str]:
+        tokens: List[str] = []
+        current_token = ""
+
+        for i in range(len(expression)):
+            character = expression[i]
+
+            if character not in TypeFormulaParser.PUNCTUATION:
+                # Non-punctuation character
+                current_token += character
+            else:
+                if current_token:
+                    tokens.append(current_token.strip())
+                    current_token = ""
+                # Punctuation character
+                tokens.append(character)
+
+        if current_token:
+            tokens.append(current_token.strip())
+
+        return tokens
