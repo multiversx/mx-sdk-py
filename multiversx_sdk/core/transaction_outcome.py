546387b6aa237884f7393d0014b712d433aeeb90
Alexandru Popenta <alexandru.p9913@gmail.com>
2024-10-07 17:52:34 +0300

    move logs and events in transaction outcome

--- multiversx_sdk/core/transaction_outcome.py
+++ multiversx_sdk/core/transaction_outcome.py
@@ -1,32 +1,38 @@
-from typing import Callable, List
+from typing import Any, Callable
 
 
 class TransactionEvent:
     def __init__(self,
+                 raw: dict[str, Any] = {},
                  address: str = "",
                  identifier: str = "",
-                 topics: List[bytes] = [],
-                 data_items: List[bytes] = []) -> None:
+                 topics: list[bytes] = [],
+                 data: bytes = b"",
+                 additional_data: list[bytes] = []) -> None:
+        self.raw = raw
         self.address = address
         self.identifier = identifier
         self.topics = topics
-        self.data_items = data_items
+        self.data = data
+        self.additional_data = additional_data
 
 
 class TransactionLogs:
     def __init__(self,
                  address: str = "",
-                 events: List[TransactionEvent] = []) -> None:
+                 events: list[TransactionEvent] = []) -> None:
         self.address = address
         self.events = events
 
 
 class SmartContractResult:
     def __init__(self,
+                 raw: dict[str, Any] = {},
                  sender: str = "",
                  receiver: str = "",
                  data: bytes = b"",
                  logs: TransactionLogs = TransactionLogs()) -> None:
+        self.raw = raw
         self.sender = sender
         self.receiver = receiver
         self.data = data
@@ -36,7 +42,7 @@ class SmartContractResult:
 class SmartContractCallOutcome:
     def __init__(self,
                  function: str = "",
-                 return_data_parts: List[bytes] = [],
+                 return_data_parts: list[bytes] = [],
                  return_message: str = "",
                  return_code: str = "") -> None:
         self.function = function
@@ -48,18 +54,18 @@ class SmartContractCallOutcome:
 class TransactionOutcome:
     def __init__(self,
                  direct_smart_contract_call_outcome: SmartContractCallOutcome = SmartContractCallOutcome(),
-                 transaction_results: List[SmartContractResult] = [],
+                 transaction_results: list[SmartContractResult] = [],
                  transaction_logs: TransactionLogs = TransactionLogs()) -> None:
         self.direct_smart_contract_call = direct_smart_contract_call_outcome
         self.transaction_results = transaction_results
         self.logs = transaction_logs
 
 
-def find_events_by_identifier(transaction_outcome: TransactionOutcome, identifier: str) -> List[TransactionEvent]:
-    return find_events_by_predicate(transaction_outcome, lambda event: event.identifier == identifier)
+def find_events_by_identifier(transaction_outcome: TransactionOutcome, identifier: str) -> list[TransactionEvent]:
+    return _find_events_by_predicate(transaction_outcome, lambda event: event.identifier == identifier)
 
 
-def find_events_by_first_topic(transaction_outcome: TransactionOutcome, topic: str) -> List[TransactionEvent]:
+def find_events_by_first_topic(transaction_outcome: TransactionOutcome, topic: str) -> list[TransactionEvent]:
     def is_topic_matching(event: TransactionEvent):
         if not len(event.topics):
             return False
@@ -70,18 +76,18 @@ def find_events_by_first_topic(transaction_outcome: TransactionOutcome, topic: s
         except UnicodeDecodeError:
             return False
 
-    return find_events_by_predicate(transaction_outcome, is_topic_matching)
+    return _find_events_by_predicate(transaction_outcome, is_topic_matching)
 
 
-def find_events_by_predicate(
+def _find_events_by_predicate(
     transaction_outcome: TransactionOutcome,
     predicate: Callable[[TransactionEvent], bool]
-) -> List[TransactionEvent]:
-    events = gather_all_events(transaction_outcome)
+) -> list[TransactionEvent]:
+    events = _gather_all_events(transaction_outcome)
     return list(filter(predicate, events))
 
 
-def gather_all_events(transaction_outcome: TransactionOutcome) -> List[TransactionEvent]:
+def _gather_all_events(transaction_outcome: TransactionOutcome) -> list[TransactionEvent]:
     all_events = [*transaction_outcome.logs.events]
 
     for result in transaction_outcome.transaction_results:
