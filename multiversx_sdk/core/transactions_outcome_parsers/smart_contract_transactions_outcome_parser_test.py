470029d8fb09bcf39227b71c25a684dccccbf8aa
Alexandru Popenta <alexandru.p9913@gmail.com>
2024-10-17 11:52:56 +0300

    add more unit tests

--- multiversx_sdk/core/transactions_outcome_parsers/smart_contract_transactions_outcome_parser_test.py
+++ multiversx_sdk/core/transactions_outcome_parsers/smart_contract_transactions_outcome_parser_test.py
@@ -1,5 +1,9 @@
+import re
+from pathlib import Path
+
 import pytest
 
+from multiversx_sdk.abi.abi import Abi
 from multiversx_sdk.core.address import Address
 from multiversx_sdk.core.transaction_on_network import (SmartContractResult,
                                                         TransactionEvent,
@@ -87,6 +91,35 @@ class TestSmartContractTransactionsOutcomeParser:
         assert len(parsed.contracts) == 0
         assert parsed.contracts == []
 
+    def test_parse_execute_outcome_with_abi(self):
+        abi_path = Path(__file__).parent.parent.parent / "testutils" / "testdata" / "answer.abi.json"
+        abi = Abi.load(abi_path)
+        parser = SmartContractTransactionsOutcomeParser(abi)
+
+        transaction = TransactionOnNetwork()
+        transaction.function = "getUltimateAnswer"
+        transaction.contract_results = [
+            SmartContractResult(data="@6f6b@2a".encode())
+        ]
+
+        parsed_tx = parser.parse_execute(transaction)
+        assert parsed_tx.return_code == "ok"
+        assert parsed_tx.return_message == "ok"
+        assert parsed_tx.values == [42]
+
+    def test_parse_execute_without_function_name(self):
+        abi_path = Path(__file__).parent.parent.parent / "testutils" / "testdata" / "answer.abi.json"
+        abi = Abi.load(abi_path)
+        parser = SmartContractTransactionsOutcomeParser(abi)
+
+        transaction = TransactionOnNetwork()
+        transaction.contract_results = [
+            SmartContractResult(data="@6f6b@2a".encode())
+        ]
+
+        with pytest.raises(Exception, match=re.escape('Function name is not available in the transaction, thus endpoint definition (ABI) cannot be picked (for parsing). Please provide the "function" parameter explicitly.')):
+            parser.parse_execute(transaction)
+
     @pytest.mark.networkInteraction
     def test_parse_successful_deploy(self):
         successful_tx_hash = "30bc4f262543e235b73ae6db7bcbf3a54513fe3c1ed7a86af688a8f0e7fe8655"
@@ -108,3 +141,18 @@ class TestSmartContractTransactionsOutcomeParser:
         parsed = self.parser.parse_deploy(tx_on_network)
         assert len(parsed.contracts) == 0
         assert parsed.contracts == []
+
+    @pytest.mark.networkInteraction
+    def test_parse_sc_call(self):
+        tx_hash = "8b599aa57d456aab573fcc1d5f409d4d00b897edbe1b7522a00604c0d64ea6cd"
+        proxy = ProxyNetworkProvider("https://devnet-gateway.multiversx.com")
+
+        tx_on_network = proxy.get_transaction(tx_hash)
+
+        parsed = self.parser.parse_execute(tx_on_network, "getActionSigners")
+        assert parsed.return_code == "ok"
+        assert parsed.return_message == "ok"
+        assert len(parsed.values) == 1
+
+        expected_value = 2
+        assert parsed.values == [expected_value.to_bytes()]
