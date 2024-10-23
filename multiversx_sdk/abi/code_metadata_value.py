b281dd6a680baa3ca2d838eba1bf8f5b82b7c1de
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-10-23 10:29:15 +0300

    ABI: handle CodeMetadataValue.

---
+++ multiversx_sdk/abi/code_metadata_value.py
@@ -0,0 +1,46 @@
+import io
+from typing import Any
+
+from multiversx_sdk.abi.shared import read_bytes_exactly
+from multiversx_sdk.core.code_metadata import (CODE_METADATA_LENGTH,
+                                               CodeMetadata)
+
+
+class CodeMetadataValue:
+    def __init__(self, value: bytes = b"") -> None:
+        self.value = value
+
+    @classmethod
+    def from_code_metadata(cls, code_metadata: CodeMetadata) -> "CodeMetadataValue":
+        return cls(code_metadata.serialize())
+
+    def encode_nested(self, writer: io.BytesIO):
+        writer.write(self.value)
+
+    def encode_top_level(self, writer: io.BytesIO):
+        writer.write(self.value)
+
+    def decode_nested(self, reader: io.BytesIO):
+        length = CODE_METADATA_LENGTH
+        data = read_bytes_exactly(reader, length)
+        self.value = data
+
+    def decode_top_level(self, data: bytes):
+        self.value = data
+
+    def set_payload(self, value: Any):
+        if isinstance(value, bytes):
+            self.value = CodeMetadata.from_bytes(value).serialize()
+        elif isinstance(value, CodeMetadata):
+            self.value = value.serialize()
+        else:
+            raise ValueError(f"cannot set payload for code metadata (should be either a CodeMetadata or bytes, but got: {type(value)})")
+
+    def get_payload(self) -> Any:
+        return self.value
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, CodeMetadataValue) and self.value == other.value
+
+    def __bytes__(self) -> bytes:
+        return self.value
