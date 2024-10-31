c6ca4504d10ab46675245227dc0f7fe9c8c6b30a
Alexandru Popenta <alexandru.p9913@gmail.com>
2024-10-31 16:33:32 +0200

    fix imports

--- multiversx_sdk/core/transactions_outcome_parsers/delegation_transactions_outcome_parser.py
+++ multiversx_sdk/core/transactions_outcome_parsers/delegation_transactions_outcome_parser.py
@@ -1,12 +1,12 @@
 from typing import List
 
 from multiversx_sdk.core.address import Address
+from multiversx_sdk.core.constants import DEFAULT_HRP
 from multiversx_sdk.core.errors import ParseTransactionOnNetworkError
 from multiversx_sdk.core.transaction_on_network import (
     TransactionEvent, TransactionOnNetwork, find_events_by_identifier)
 from multiversx_sdk.core.transactions_outcome_parsers.delegation_transactions_outcome_parser_types import \
     CreateNewDelegationContractOutcome
-from multiversx_sdk.network_providers.constants import DEFAULT_ADDRESS_HRP
 
 
 class DelegationTransactionsOutcomeParser:
@@ -32,4 +32,4 @@ class DelegationTransactionsOutcomeParser:
     def _extract_contract_address(self, event: TransactionEvent) -> str:
         if not event.topics[0]:
             return ""
-        return Address(event.topics[0], DEFAULT_ADDRESS_HRP).to_bech32()
+        return Address(event.topics[0], DEFAULT_HRP).to_bech32()
