7315a635c1c983eae2a7ebaa24aab607d68373c5
Alexandru Popenta <alexandru.p9913@gmail.com>
2024-11-29 11:25:17 +0200

    small fixes

--- multiversx_sdk/core/config.py
+++ multiversx_sdk/core/config.py
@@ -6,11 +6,11 @@ class LibraryConfig:
     """
     Global configuration of the library.
 
-    Generally speaking, this configuration should only be altered on exotic use cases
-    it can be seen as a collection of constants (or , to be more precise, rarely changed variables) that are used throughout the library.
+    Generally speaking, this configuration should only be altered in exotic use cases.
+    It can be seen as a collection of constants or, more precisely, variables that are rarely changed and used throughout the library.
 
     Never alter the configuration within a library!
-    Only alter the configuration(if needed) within an(end) application that uses this library.
+    Only alter the configuration, if needed, within a final application that uses this library.
     """
 
     # The human-readable part of the bech32 addresses
