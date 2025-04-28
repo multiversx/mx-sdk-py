4bfaccda589e377b1e79185b95626131577b1864
Alexandru Popenta <alexandru.p9913@gmail.com>
2025-04-28 10:41:19 +0300

    add is_failed for transaction status

--- multiversx_sdk/core/transaction_status.py
+++ multiversx_sdk/core/transaction_status.py
@@ -6,19 +6,21 @@ class TransactionStatus:
     status: str
     is_completed: bool
     is_successful: bool
+    is_failed: bool
 
     def __init__(self, status: str):
         self.status = status.lower()
         self.is_completed = self._is_status_completed()
         self.is_successful = self._is_status_successful()
+        self.is_failed = self._is_status_failed()
 
     def _is_status_completed(self) -> bool:
-        return self._is_status_successful() or self._is_failed()
+        return self._is_status_successful() or self._is_status_failed()
 
     def _is_status_successful(self) -> bool:
         return self.status == "executed" or self.status == "success" or self.status == "successful"
 
-    def _is_failed(self) -> bool:
+    def _is_status_failed(self) -> bool:
         return (
             self.status == "fail"
             or self.status == "failed"
