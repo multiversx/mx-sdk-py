5e2138112e0580f360d8c2f118cf7601447a0b66
Alexandru Popenta <alexandru.p9913@gmail.com>
2025-05-07 17:03:40 +0300

    inherit network providers error

--- multiversx_sdk/network_providers/errors.py
+++ multiversx_sdk/network_providers/errors.py
@@ -18,8 +18,8 @@ class ExpectedAccountConditionNotReachedError(Exception):
         super().__init__("The expected account condition was not reached")
 
 
-class TransactionFetchingError(Exception):
+class TransactionFetchingError(NetworkProviderError):
     def __init__(self, url: str, error: Any):
-        super().__init__(f"Couldn't fetch transaction on url = [{url}], error = [{error}]")
+        super().__init__(url, error)
         self.url = url
         self.data = error
